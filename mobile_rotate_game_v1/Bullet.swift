5a2993eaa744e19e70c25d4fc4f6d45711cf20f2
Wanaka <jonas.haag@hotmail.com>
2017-11-21 17:16:41 +0100

    First message

---
+++ mobile_rotate_game_v1/Bullet.swift
@@ -0,0 +1,14 @@
+//
+//  Bullet.swift
+//  mobile_rotate_game_v1
+//
+//  Created by Jonas Haag on 2017-11-21.
+//  Copyright © 2017 Jonas Haag. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Bullet : SKSpriteNode{
+
+}
